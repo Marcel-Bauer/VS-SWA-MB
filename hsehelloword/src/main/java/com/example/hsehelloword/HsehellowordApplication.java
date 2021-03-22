7ff86d18ae447b91a214bc85c514ad277ca33d4e
Marcel Bauer <BauerMar@hotmail.de>
2021-03-22 18:03:29 +0100

    Hallo
    ....

--- hsehelloword/src/main/java/com/example/hsehelloword/HsehellowordApplication.java
+++ hsehelloword/src/main/java/com/example/hsehelloword/HsehellowordApplication.java
@@ -2,12 +2,26 @@ package com.example.hsehelloword;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class HsehellowordApplication {
+
+	@GetMapping()
+	public String Hans() {
+		return "HURENSPOHN!!!";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(HsehellowordApplication.class, args);
 	}
 
+	
+
 }
+
+
