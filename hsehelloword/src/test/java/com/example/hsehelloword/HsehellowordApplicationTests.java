3d7e01a4b847ab352829215427eee3f2a2b90378
Marcel Bauer <BauerMar@hotmail.de>
2021-03-22 16:09:12 +0100

    Test1

---
+++ hsehelloword/src/test/java/com/example/hsehelloword/HsehellowordApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hsehelloword;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HsehellowordApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
